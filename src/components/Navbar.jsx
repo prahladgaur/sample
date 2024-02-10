7b331da819ecdf4c41f92c563952fc1cc3a7f95f
Prahlad Gaur <65204011+prahladgaur@users.noreply.github.com>
2024-02-10 19:19:51 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-//import React from "react";
+import {useState} from "react";
 import { Link ,useNavigate} from "react-router-dom";
 //import {useSelector} from 'react-redux'
 import axios from "axios"
@@ -6,6 +6,7 @@ import axios from "axios"
 function Navbar() {
   
   const navigate = useNavigate()
+   const [menuOpen, setMenuOpen] = useState(false);
   const handleLogout = () =>{
       axios.get('https://mernappback-qhiy.onrender.com/auth/logout')
       .then(res=>{
@@ -17,31 +18,52 @@ function Navbar() {
       })
   }
   return (
-    <nav>
-      <ul>
+   <>
+   <header>
+        <nav>
+          <div className="container">
 
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-       
-          
-           <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li><button 
-    className='btn btn-default border w-100 bg-light rounded-4 text-decoration-none'
-    onClick={handleLogout}
-    >LOGOUT</button></li>
-       
-       
-      </ul>
-    </nav>
+            <div className="menu-toggle" onClick={()=>{setMenuOpen(!menuOpen)}}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <ul className={`nav-links ${menuOpen ? 'open' : ''}`} >
+              <li>
+                <a href="#home"><Link to="/">Home</Link></a>
+              </li>
+              <li>
+                <a href="#about">About Us</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#work">Our Work</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                {" "}
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li>
+                <button
+                  className="btn btn-default border w-100 bg-light rounded-4 text-decoration-none"
+                  onClick={handleLogout}
+                >
+                  LOGOUT
+                </button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+   </>
   );
 }
 
